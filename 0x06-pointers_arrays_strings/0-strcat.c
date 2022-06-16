4758c430010302139dd0bf7c66d7ffe6cd25776e
Harbike <adenlekehny@gmail.com>
2022-06-16 11:06:57 -0700

    low-level- more pointers, arrays, and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,9 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *_strcat - concatenates two strings
- *@dest: input value
- *@src: input value
+ * @dest: input value
+ * @src: input value
  *
  *Return: void
  */
@@ -10,7 +11,6 @@ char *_strcat(char *dest, char *src)
 {
 int i;
 int j;
-
 i = 0;
 while (dest[i] != '\0')
 {
